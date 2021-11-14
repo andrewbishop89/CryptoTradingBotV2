18bf99f27127bff4ef5ca2b5dffbef61e0a4b12a
Andrew Bishop <bishopandrew49@gmail.com>
2021-11-13 17:31:45 -0800

    added bull flag pattern (not tested)

--- analysis.py
+++ analysis.py
@@ -13,6 +13,10 @@ from parameters import *
 
 # functions
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+# indicators
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 #PARAM start_price(float): start point for fibonacci retracement levels
 #PARAM end_price(float): end point for fibonacci retracement levels
@@ -26,3 +30,22 @@ def fibonacci_retracement_levels(
     price_difference = end_price-start_price
     return [(end_price - (price_difference*percentage)) for percentage in \
         level_percentages]
+
+# chart patterns
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+#PARAM klines(DataFrame): 5 candles in a row
+#RETURN (bool): true if bull flag was just formed, false otherwise
+#DESCRIPTION: 5 candles, low_0 < lows_1,2,3 and high_1 > high_2 > high_3 and 
+# then close_4 > high_3
+def bull_flag(klines: pd.DataFrame):
+    lows = klines[:, 'l']
+    highs = klines[:, 'h']
+    close_4 = klines[4, 'c']
+    
+    criteria_1 = (lows[0] < lows[1]) and (lows[0] < lows[2]) \
+        and (lows[0] < lows[3])
+    criteria_2 = (highs[1] > highs[2]) and (highs[2] > highs[3])
+    criteria_3 = (close_4 > highs[3])
+    
+    return (criteria_1 and criteria_2 and criteria_3)
