50f26dbc77a6d4b6a7648aca387d53cb53732edb
Andrew Bishop <bishopandrew49@gmail.com>
2021-11-27 10:57:04 -0800

    added trade loop function

--- method_1.py
+++ method_1.py
@@ -20,6 +20,37 @@ from parameters import *
 # functions
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
+def trade_loop(symbol: str, interval: str):
+    
+    buy_id, sell_quantity = buy_trade(symbol, 40) #buy in
+    
+    price_fluctuation = 0.1
+    
+    buy_price = current_price_f(symbol)
+
+    max_price = buy_price
+    stop_price = buy_price*(1-price_fluctuation)
+    
+    while True:
+        klines = download_to_csv(symbol, interval)
+        current_price = klines.iloc[-1]['c']
+        current_high = klines.iloc[-1]['h']
+        
+        max_price = max(max_price, current_high)
+        
+        if (current_price < stop_price): # if below stop loss take losses
+            sell_trade(symbol, quantity=sell_quantity)
+            sell_price = current_price_f(symbol)
+            print(f"PROFIT: {round((1-sell_price/buy_price)*100, 2)}%")
+            break
+        
+        if (max_price > ((1+price_fluctuation/2)*buy_price)):
+            price_fluctuation = max_price - buy_price*price_fluctuation #top value - 10% buy in
+        
+        time.sleep(45)
+        
+    return
+
 def main():
     gain_threshold_value = 10 #gain required in 5min period for buy in
     larger_interval = '5m'
