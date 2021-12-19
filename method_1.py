8c9bcc2ac134f441b8c0b7485ecd8607a1a6abb3
Andrew Bishop <bishopandrew49@gmail.com>
2021-12-19 08:30:58 -0800

    reformatted print

--- method_1.py
+++ method_1.py
@@ -57,7 +57,7 @@ def trade_loop(
             sell_price = current_price_f(symbol)
             profit = round((sell_price/buy_price-1)*100, 2)
             profit_color = GREEN if profit > 0 else RED
-            print(f"{BLUE}CRITERIA ACHIEVED{WHITE} selling {symbol}.")
+            print(f"{profit_color}CRITERIA ACHIEVED{WHITE} selling {symbol}.")
             print(f"{GREY}SELL PRICE{WHITE}: {sell_price}")
             end_time = time.time()
             print(f"End: {get_time(end_time-8*3600)} - {end_time}\n", end='\r')
