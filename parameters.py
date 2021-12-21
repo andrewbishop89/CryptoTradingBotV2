c895092bf3e21896f0e81692e2829f561343e9a9
Andrew Bishop <bishopandrew49@gmail.com>
2021-12-21 11:07:36 -0800

    cleaned up code

--- parameters.py
+++ parameters.py
@@ -1,48 +1,10 @@
 #!/usr/bin/python3
 #
-# parameters.py: contains all imported modules and global variables.
+# parameters.py: contains all API variables and other global variables.
 #
 # Andrew Bishop
-# 2021/11/13
-#
 
-# modules imported
-"""
-from functools import lru_cache
-import hmac
-import hashlib
-import requests
-import websocket
-import threading
-import json
-from binance.client import Client
-import csv
-from urllib.parse import urlencode
-import math
-import os
-import itertools
-import pandas as pd
-import numpy as np
-import time
-import pync
-import curses
-import sys
-import shutil
-import time
-from pprint import pprint, pformat
-from datetime import datetime
-
-# my modules
-from api import *
-from backtest import *
-from data_collection import *
-from market import *
-from setup import *
-from trade import *
-from websocket import *
-"""
-
-# global variables
+#global variables
 BLUE    = '\u001b[1;38;5;$39m'
 YELLOW  = '\u001b[38;5;$220m'
 GREEN   = '\u001b[38;5;$46m'
@@ -53,19 +15,11 @@ WHITE   = '\033[0;37m'
 PRIMARY_COLOR   = BLUE
 SECONDARY_COLOR = GREY
 
-#PARAM real(bool): True for real API, False for paper money
-#RETURN none
-#def init_api_keys(real=True):
-global API_KEY, API_SECRET, BASE_URL
+#initialize API values
+global API_KEY, API_SECRET, BASE_URL #global so they are accessible everywhere
 
 with open('keys.txt', 'r') as f:
-    lines = f.readlines()
-    #NOTE these are for the binance API
-    #if real == False:
-    #    API_KEY = lines[0][:-1]
-    #    API_SECRET = lines[1][:-1]
-    #    BASE_URL = "https://testnet.binance.vision"
-    #elif real == True:
-    API_KEY = lines[3][:-1]
-    API_SECRET = lines[4][:-1]
-    BASE_URL = "https://api.binance.com"
+    lines = f.readlines() #read from keys.txt file
+    API_KEY = lines[0][:-1] #first line should be API key
+    API_SECRET = lines[1][:-1] #second line should be API secret
+    BASE_URL = "https://api.binance.com" #binance API base url
