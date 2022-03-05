125f65f05ad824c4b55925152d63fc1b55c1b0de
Andrew Bishop <bishopandrew49@gmail.com>
2022-03-04 19:48:12 -0800

    added daily ticker func

--- market.py
+++ market.py
@@ -15,6 +15,26 @@ from api import *
 
 #----------------------------------functions-----------------------------------
 
+def daily_ticker_24hr(symbol:str=None):
+    """
+    Description:
+        Returns DataFrame of symbol(s) of 24h price data.
+    Args:
+        symbol (str, optional): symbol of coin to return (defaults to None)
+    Returns:
+        pd.DataFrame: DataFrame of symbol(s) of all 24h price data. 
+    """
+    tickers = send_public_request('/api/v3/ticker/24hr')
+    df = pd.DataFrame.from_dict(tickers)
+    df = df[df['symbol'].str.contains('USDT')]
+    #df = df[df['symbol'].str.contains('DOWN') == False]
+    #df = df[df['symbol'].str.contains('UP') == False]
+    df = df.set_index('symbol')
+    if symbol:
+        return df.loc[symbol]
+    else:
+        return df
+
 def top_gainers(min_percent: float = 0, 
     max_percent: float = 0) -> pd.DataFrame:
     """
