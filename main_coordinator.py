d098dcebff7e7019b63d99e1dfbaf3b2f0f579bf
Andrew Bishop <bishopandrew49@gmail.com>
2022-05-11 22:46:26 -0700

    added main file

---
+++ main_coordinator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#
+# main_coordinator.py: executes the method specified from the system arguments 
+# or the internal variables.
+#
+# Andrew Bishop
+# 2022/05/11
+#
+
+import methods.method_2.main_2 as m
+
+if __name__ == "__main__":
+    m.main()
