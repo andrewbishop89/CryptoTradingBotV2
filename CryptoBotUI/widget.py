07879c3495efbb9c6074d4e7b6c46cb47da8a7b6
Andrew Bishop <bishopandrew49@gmail.com>
2022-05-24 09:39:28 -0700

    base ui

---
+++ CryptoBotUI/widget.py
@@ -0,0 +1,16 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from PySide6.QtWidgets import QApplication, QWidget
+
+
+class Widget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Widget()
+    window.show()
+    sys.exit(app.exec_())
+
