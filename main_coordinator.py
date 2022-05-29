d9db417b97f784e857052e1ac5771868e3d083a2
Andrew Bishop <bishopandrew49@gmail.com>
2022-05-28 17:36:15 -0700

    moved parameters to main function

--- main_coordinator.py
+++ main_coordinator.py
@@ -7,7 +7,21 @@
 # 2022/05/11
 #
 
-import methods.method_2.main_2 as m
+import sys
+import logging
+from methods.method_2.main_2 import main
+from helper_functions.market import *
 
 if __name__ == "__main__":
-    m.main()
+    logger = logging.getLogger("main")
+    LOGGING_LEVEL = logging.DEBUG
+
+    logger.setLevel(LOGGING_LEVEL)
+
+    #symbols = ["BTCUSDT", "ADAUSDT", "ETHUSDT", "TRXUSDT", "SOLUSDT", "LTCUSDT", "XRPUSDT", "XMRUSDT", "DOTUSDT"]
+    #symbols = top_gainers(2).index#[-25:]
+    num_symbols = int(sys.argv[1]) if (len(sys.argv) > 1) else 50
+    symbols = top_volume_gainers(num_symbols).index
+    #symbols = symbols.drop_duplicates()
+
+    main(symbols, 12)
