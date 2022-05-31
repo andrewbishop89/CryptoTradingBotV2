ff685327fc68b49a42d93b2f3ce2c3d29cf28c41
Andrew Bishop <bishopandrew49@gmail.com>
2022-05-31 09:42:16 -0700

    added potential profit

--- methods/method_2/method_2_func.py
+++ methods/method_2/method_2_func.py
@@ -111,6 +111,7 @@ def log_profits(
         max_price,
         max_profit,
         max_stop,
+        potential_profit,
         file_lock: threading.Lock, 
         real: bool=False):
     """
@@ -159,10 +160,10 @@ def log_profits(
             f.write("profit,symbol,buy_price,sell_price,buy_time," + \
                 "sell_time,side,profit_split_ratio,std_5m," + \
                 "difference_1h,price_24h,volume_24h,volume_rel," + \
-                "risk_multiplier,min_price,max_price\n")
+                "risk_multiplier,min_price,max_price,max_profit,max_stop,potential_profit\n")
     with open(fp, "a") as f:
         f.write(f"{profit},{symbol},{buy_price},{sell_price}," + \
             f"{buy_time},{sell_time},{side},{profit_split_ratio}," + \
             f"{std_5m},{difference_1h},{price_24h},{volume_24h}," + \
-            f"{volume_rel},{risk_multiplier},{min_price},{max_price}\n")
+            f"{volume_rel},{risk_multiplier},{min_price},{max_price},{max_profit},{max_stop},{potential_profit}\n")
     file_lock.release()
