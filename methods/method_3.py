8e7628ad427d33a75d10a5840dce235c5b30d9e2
Andrew Bishop <bishopandrew49@gmail.com>
2022-08-02 18:47:20 -0700

    added test method

--- methods/method_3.py
+++ methods/method_3.py
@@ -8,8 +8,22 @@
 
 from helper_functions import analysis
 
+
 def buy_conditions(klines) -> bool:
-    return 
+        
+    currnet_price = klines["5m"].loc[-1, "c"]
+        
+    ema_21 = analysis.EMA(klines["5m"], 21)[-1]
+    ema_50 = analysis.EMA(klines["5m"], 50)[-1]
+    ema_100 = analysis.EMA(klines["5m"], 100)[-1]
+    
+    return (ema_21 > ema_50) and (ema_50 > ema_100) 
+
     
 def sell_conditions(klines) -> bool:
+    
+    currnet_price = klines["5m"].loc[-1, "c"]
+
+    #TODO include risk multiplier, buy price and other sell conditions in here
+    
     return
