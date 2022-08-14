707305ebd6c773a5c8c550cced5b4391f730084d
Andrew Bishop <bishopandrew49@gmail.com>
2022-08-14 15:11:04 -0700

    reorganized project structure

--- helper_functions/kraken/stake.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-#
-# stake.py: contains all functions for staking with the kraken api
-#
-# Andrew Bishop
-# 2022/05/30
-#
--- notes.md
+++
@@ -1,6 +0,0 @@
-# Notes
-
-**Current Tasks**
-- New_Method_Framework
-  - Need to restructure framework
-- CLI
