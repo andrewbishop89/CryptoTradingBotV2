09aa4ed65100dbe035c0fc1e0cf313c1bc013199
Andrew Bishop <bishopandrew49@gmail.com>
2022-10-30 22:52:04 -0700

    fixed general api functionality

--- api/general_api.py
+++ api/general_api.py
@@ -12,43 +12,10 @@ import hashlib
 import hmac
 from dataclasses import dataclass
 from urllib.parse import urlencode
+from rich.pretty import pprint
 
 
-def _hashing(query_string: str) -> str:
-    """
-    Description:
-        Hashes the string inputted as argument using SHA-256.
-    Args:
-        query_string (str): query string for api request
-    Returns:
-        str: hashed string for api requests
-    """
-    return hmac.new(
-        API_SECRET.encode('utf-8'),
-        query_string.encode('utf-8'),
-        hashlib.sha256).hexdigest()
-
-
-def _dispatch_request(http_method: str) -> dict:
-    """
-    Description:
-        _dispatches an API request by getting ready to be sent.
-    Args:
-        http_method (str): http method for request
-    Returns:
-        dict: api request status
-    """
-    session = requests.Session()
-    session.headers.update({
-        'Content-Type': 'application/json;charset=utf-8',
-        'X-MBX-APIKEY': API_KEY
-    })
-    return {
-        'GET': session.get,
-        'DELETE': session.delete,
-        'PUT': session.put,
-        'POST': session.post,
-    }.get(http_method, 'GET')
+from functions.setup.setup import get_timestamp
 
 
 @dataclass
@@ -60,8 +27,53 @@ class API:
     API_SECRET: str
     BASE_URL: str
 
+    # ----------------------------------------------------------
+    #                      Helper Functions
+    # ----------------------------------------------------------
 
-    def send_signed_request(http_method: str, url_path: str, payload: dict = None) -> dict:
+    def _hashing(self, query_string: str) -> str:
+        """
+        Description:
+            Hashes the string inputted as argument using SHA-256.
+        Args:
+            query_string (str): query string for api request
+        Returns:
+            str: hashed string for api requests
+        """
+        return hmac.new(
+            self.API_SECRET.encode('utf-8'),
+            query_string.encode('utf-8'),
+            hashlib.sha256).hexdigest()
+
+
+    def _dispatch_request(self, http_method: str) -> dict:
+        """
+        Description:
+            _dispatches an API request by getting ready to be sent.
+        Args:
+            http_method (str): http method for request
+        Returns:
+            dict: api request status
+        """
+        session = requests.Session()
+        session.headers.update({
+            'Content-Type': 'application/json;charset=utf-8',
+            'X-MBX-APIKEY': self.API_KEY
+        })
+        return {
+            'GET': session.get,
+            'DELETE': session.delete,
+            'PUT': session.put,
+            'POST': session.post,
+        }.get(http_method, 'GET')
+
+
+    # ----------------------------------------------------------
+    #                     Main API Functions
+    # ----------------------------------------------------------
+
+
+    def send_signed_request(self, http_method: str, url_path: str, payload: dict = None) -> dict:
         """
         Description:
             Creates a signed API request for specified parameters.
@@ -85,17 +97,17 @@ class API:
         else:
             query_string = 'timestamp={}'.format(get_timestamp())
 
-        url = BASE_URL + url_path + '?' + query_string + '&signature=' + _hashing(query_string)
+        url = self.BASE_URL + url_path + '?' + query_string + '&signature=' + self._hashing(query_string)
         params = {'url': url, 'params': {}}
         try:
-            response = _dispatch_request(http_method)(**params)
+            response = self._dispatch_request(http_method)(**params)
         except ConnectionError:
-            print(f"{RED}ERROR{WHITE}: Could not establish a connection.")
+            print(f"[red]ERROR[/]: Could not establish a connection.")
             raise ConnectionError
         return response.json()
 
 
-    def send_public_request(url_path: str, payload: dict = None) -> dict:
+    def send_public_request(self, url_path: str, payload: dict = None) -> dict:
         """
         Description:
             Creates a public API request for specified parameters.
@@ -116,9 +128,11 @@ class API:
         if query_string:
             url = url + '?' + query_string
         try:
-            response = _dispatch_request('GET')(url=url)
+            response = self._dispatch_request('GET')(url=url)
         except ConnectionError:
-            print(f"{RED}ERROR{WHITE}: Could not establish a connection.")
+            print(f"[red]ERROR[/]: Could not establish a connection.")
             raise ConnectionError
         return response.json()
+
+
 
